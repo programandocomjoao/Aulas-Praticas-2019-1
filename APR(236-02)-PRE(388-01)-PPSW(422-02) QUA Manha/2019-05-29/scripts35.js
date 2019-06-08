59b5a44a83390e8e1b926d4538f00a40b1a1078d
programandocomjoao <46997033+programandocomjoao@users.noreply.github.com>
2019-06-07 21:37:44 -0300

    Add files via upload

---
+++ APR(236-02)-PRE(388-01)-PPSW(422-02) QUA Manha/2019-05-29/scripts35.js
@@ -0,0 +1,11 @@
+function exibirData() {
+	document.write(Date());
+}
+
+function exibirResolucao() {
+	document.write(screen.width + "x" + screen.height);
+}
+
+function exibirAlturaDisp() {
+	window.alert("Altura de tela disponível: " + screen.availHeight);
+}
